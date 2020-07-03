70466fe969175f1e25616613fa94014c9761d738
SoujanyaChatti <67154494+SoujanyaChatti@users.noreply.github.com>
2020-07-03 16:11:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -27,7 +27,7 @@ function setup() {
 	paper=new Paper(50,580);
     
 	ground2=new Groundd(370,585,740,20);
-	ground3=new Groundd(850,565,200,20);
+	ground3=new Groundd(850,550,200,20);
 	ground4=new Groundd(760,505,5,180);
 	box1=new Box(850,585,180,20);
 	box2=new Box(750,505,20,180);
