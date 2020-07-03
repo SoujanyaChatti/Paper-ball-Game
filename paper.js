40afb4b94dec5de8ef4aaba59a06ec421b2b8b21
SoujanyaChatti <67154494+SoujanyaChatti@users.noreply.github.com>
2020-07-03 13:46:48 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,24 @@
+class Paper{
+    constructor(x,y){
+        var options={
+          'isStatic':false,
+          'restitution':0.3,
+          'friction':0.5,
+          'density':1.2
+        }
+        this.body=Matter.Bodies.circle(x,y,30,options);
+        this.radius=30;
+        
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        ellipseMode(RADIUS);
+        fill("yellow");
+        ellipse(pos.x,pos.y,this.radius);
+        
+    }
+  
+
+    }
+   
---
+++ sketch.js
@@ -0,0 +1,67 @@
+var ground,paper,dustbin,pl;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	pl=loadImage("redball.jpg");
+}
+
+function setup() {
+	createCanvas(1000, 700);
+    
+    
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	paper=new Paper(50,50);
+
+	papersprite=createSprite(200,300, 10,10);
+	papersprite.addImage(pl);
+	papersprite.scale=0.5;
+
+	ground=new Ground(500,620,1000,50);
+	paper=new Paper(50,580);
+    
+	ground2=new Groundd(370,585,740,20);
+	
+	
+	box1=new Box(850,585,180,20);
+	box2=new Box(750,505,20,180);
+	box3=new Box(950,505,20,180);
+}
+
+
+function draw() {
+rectMode(CENTER);
+ellipseMode(RADIUS);
+  background(0);
+  Engine.update(engine);
+  paper.display();
+  ground.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  ground2.display();
+  fill("yellow");
+  text("USE THE CONTROL KEYS TO THROW THE PAPER INTO THE BIN",200,50);
+  drawSprites();
+ papersprite.x=paper.body.position.x;
+ papersprite.y=paper.body.position.y;
+}
+
+function keyPressed(){
+	if(keyCode==UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:160,y:-160});
+	}
+	else if(keyCode==RIGHT_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:50,y:0});
+	}
+	else if(keyCode==LEFT_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.postion,{x:-50,y:0});
+	}
+}
+
